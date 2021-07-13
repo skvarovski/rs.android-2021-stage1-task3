ba869ef6a29ffc3c48ee23354c15b5d148b952d4
SkvarovskiXenon <skvarovski@gmail.com>
2021-07-14 00:17:42 +0300

    feat: telephone solution

--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -2,8 +2,44 @@ package subtask3
 
 class TelephoneFinder {
 
-    // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+
+        // создаём результирующий массив
+        var array = mutableListOf<String>()
+
+        // перебираем посимвольно входящую строку
+        number.forEach { char ->
+            // получаем массив соседних бро-номеров
+            val broNum = broNumber(char) ?: return null
+            // пробегаем по бро-номерам и делаем замену с добавление в результирующий массив
+            broNum.forEach {
+                //println(it.toString())
+                array.add(number.replace(char, it[0]))
+            }
+
+        }
+
+        return array.toTypedArray()
+
+    }
+
+    // ищем соседние бро-номера
+    private fun broNumber(num: Char): Array<String>? {
+        return when(num) {
+            '0' -> arrayOf("8")
+            '1' -> arrayOf("2","4")
+            '2' -> arrayOf("1","3","5")
+            '3' -> arrayOf("2","6")
+            '4' -> arrayOf("1","5","7")
+            '5' -> arrayOf("2","4","6","8")
+            '6' -> arrayOf("3","5","9")
+            '7' -> arrayOf("4","8")
+            '8' -> arrayOf("5","7","9","0")
+            '9' -> arrayOf("6","8")
+
+            else -> null
+
+        }
+
     }
 }
